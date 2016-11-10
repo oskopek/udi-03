54ccdd45e4c8bc7f03c4f6e9b8725a07aeef556d
Ondrej Skopek <skopekondrej@gmail.com>
2016-11-10 11:09:12 +0100

    Updated just before 03

--- findbug.py
+++ findbug.py
@@ -1,17 +1,3 @@
-####################################################
-# Utility methods
-####################################################
-
-def assertEquals(expected, actual):
-    if expected == actual:
-        print("PASS")
-    else:
-        print("FAIL: expected \"{0}\", got \"{1}\"".format(expected, actual))
-
-####################################################
-
-
-
 # factorial(n) = n * (n-1) * (n-2) * ... * 3 * 2 * 1
 def factorial(n):
     res = 0
@@ -21,6 +7,7 @@ def factorial(n):
 
 # Add your tests here
 
-assertEquals(1, factorial(1))
-assertEquals(5, factorial(120))
+def test_factiorial():
+    assert 1 == factorial(1)
+    assert 5 == factorial(120)
 
--- oh-my-not-again.py
+++ oh-my-not-again.py
@@ -1,20 +1,7 @@
-####################################################
-# Utility methods
-####################################################
-
-def assertEquals(expected, actual):
-    if expected == actual:
-        print("PASS")
-    else:
-        print("FAIL: expected \"{0}\", got \"{1}\"".format(expected, actual))
-
-####################################################
-
-
 def f(x):
     return x[-1] + f(x[:-1])
 
 # Add your tests here
-
-assertEquals("1", f("1"))
+def test_f():
+    assert "1" == f("1")
 
